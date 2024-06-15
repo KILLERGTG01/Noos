c9d9c3b76835349b518d92de141278089af6fcf6
Anurag Goel <gtg07817@gmail.com>
2024-06-15 18:15:11 +0530

    hydration sorted

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+'use client'
+import React,{useEffect,useState} from 'react'
+//import dynamic from 'next/dynamic'
 
 const Navbar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <header className='bg-white shadow-md'>
         <div className="w-22vh bg-gradient-to-r from-amber-200 to-white">
-            
+          
         </div>
     </header>
   )
