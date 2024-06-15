f4e85b1d2bda60155af6a9e140eee087e4b0dbdf
Anurag Goel <gtg07817@gmail.com>
2024-06-15 16:20:42 +0530

    Navbar sorted

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <header className='bg-white shadow-md'>
+        <div className="w-22vh bg-gradient-to-r from-amber-200 to-white">
+            
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
